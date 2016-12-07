ad30c3102a4892a7d5cd1bd635540508baf5b018
James Quiambao <james.quiambao@yahoo.ca>
2016-12-07 20:00:37 +0000

    Got app working

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 const express = require('express'); 
 const app = express(); 
-const port = process.env.PORT || 8080; 
+const port = process.env.PORT || 8080; // Set ports
+const multer = require('multer'); //Require multer module 
+
+// Initialize multer to upload files 
+app.use(multer({dest:'./uploads/'}).single('upfile'));
+var upload = multer({ dest: 'uploads/' });
+
+//Middleware 
+app.use('/', express.static(__dirname + '/public'));
+
+//Post request to get file size 
+app.post('/get-file-size', upload.single('upfile'), function(req, res){
+    // console.dir(req.files);
+    console.log(req.file.size);
+}); 
 
 //Allows user to pload and submit a file
 // Use a template engine
@@ -9,6 +23,7 @@ const port = process.env.PORT || 8080;
 // Using mutler npm
 // Recieve size of the file in JSON format
 
+// Port
 app.listen(port, function(){
     console.log("Listening on port: " + port); 
 })
