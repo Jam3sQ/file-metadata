045252ae32fea709973859012d9b5c2bd45f43ec
James Quiambao <james.quiambao@yahoo.ca>
2016-12-07 04:36:38 +0000

    Sudo code

--- server.js
+++ server.js
@@ -1,2 +1,14 @@
 const express = require('express'); 
 const app = express(); 
+const port = process.env.PORT || 8080; 
+
+//Allows user to pload and submit a file
+// Use a template engine
+// Generate a post request
+// Extract data from that post request 
+// Using mutler npm
+// Recieve size of the file in JSON format
+
+app.listen(port, function(){
+    console.log("Listening on port: " + port); 
+})
